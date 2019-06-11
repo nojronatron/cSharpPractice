061418e076451de346a3e0f29e2c06484996a559
Jon Rumsey <jon.rumsey@gmail.com>
2019-06-10 21:27:38 -0700

    In class (and take-home) exercise Stock Program (incomplete).

---
+++ Stock_Exercise/Stock_Exercise/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Stock_Exercise
+{
+    class Program
+    {
+        static string[] Companies = { "MSFT", "HP", "Compaq", "IBM", "Apple",
+                                      "Citrix", "LogMeIn", "ViewSonic", "Samsung", "Amazon",
+                                      "NetFlix", "Activision Blizzard"};
+        static Stock[] Stocks = new Stock[12];
+        static void Main(string[] args)
+        {
+            CreateArrayOfStocks();
+            DisplayStocks();
+
+            Console.WriteLine(GetStock("MSFT"));
+
+            Console.Write("Press <Enter> Key to exit. . .");
+            Console.ReadLine();
+        }
+        static Stock GetStock(string name)
+        {
+            // retreive a particular company's stock
+            foreach(Stock s in Stocks)
+            {
+                if (s.CompanyName == name)
+                {
+                    return s;
+                }
+            }
+            return null;
+        }
+        static void CreateArrayOfStocks()
+        {
+            Random rand = new Random();
+            for(int i=0; i<Stocks.Length; i++)
+            {
+                int index = rand.Next(0, Companies.Length);
+                string name = Companies[index];
+                int numShares = rand.Next(0, 100);
+                double price = rand.Next(0, 1000);
+                Stock stock = new Stock(name, numShares, price);
+                Stocks[i] = stock;
+            }
+        }
+        static void DisplayStocks()
+        {
+            Console.WriteLine($"***** Your portfolio of stocks *****");
+            foreach(Stock s in Stocks)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Stock_Exercise/Stock_Exercise/Stock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stock_Exercise
+{
+    class Stock
+    {
+        // Fields
+        private string _companyName;
+        private int _numberOfShares;
+        private double _pricePerShare;
+        // CTORs
+        public Stock() { }
+        public Stock(string companyName, int numberOfShares, double pricePerShare)
+        {
+            _companyName = companyName;
+            _numberOfShares = numberOfShares;
+            _pricePerShare = pricePerShare;
+        }
+        // Properties
+        public string CompanyName { get { return _companyName; } }
+        public int NumberOfShares { get { return _numberOfShares; } }
+        public double PricePerShare { get { return _pricePerShare; } }
+        // Methods
+        public void BuyShares(int numShares)
+        {
+            _numberOfShares += numShares;
+        }
+        public bool SellShares( int numShares)
+        {
+            if (numShares <= _numberOfShares)
+            {
+                _numberOfShares -= numShares;
+                return true;
+            }
+            return false;
+        }
+        // OverrideMethods
+        public override string ToString()
+        {
+            return $"Name: {CompanyName, -20}\tShares: {NumberOfShares, -15}\tPricePerShare: {PricePerShare:C}";
+        }
+    }
+}
