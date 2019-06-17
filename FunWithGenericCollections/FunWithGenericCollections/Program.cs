da9d8a387e3d17a21ff473f0edb59519a5c15f47
Jon Rumsey <jon.rumsey@gmail.com>
2019-06-17 09:11:37 -0700

    Homework working with Generic Collections and Observable Collections.

---
+++ FunWithGenericCollections/FunWithGenericCollections/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithGenericCollections
+{
+    class Program
+    {
+        static void UseSortedSet()
+        {
+            // make some people with different ages
+            SortedSet<Person> setOfPeople = new SortedSet<Person>(new SortPeopleByAge())
+            {
+                new Person { FirstName="Homer", LastName="Simpson", Age=47 },
+                new Person {FirstName="Marge", LastName="Simpson", Age=45 },
+                new Person {FirstName="Lisa", LastName="Simpson", Age=9 },
+                new Person {FirstName="Bart", LastName="Simpson", Age=8 }
+            };
+        // note the items are sorted by age
+        foreach (Person p in setOfPeople)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine();
+
+            // add a few new people with various ages
+            setOfPeople.Add(new Person { FirstName = "Saku", LastName = "Jones", Age = 1 });
+            setOfPeople.Add(new Person { FirstName = "Mikko", LastName = "Jones", Age = 32 });
+
+            // check to see if still sorted by age
+            foreach(Person p in setOfPeople)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine();
+        }
+        static void UseGenericQueue()
+        {
+            // Make a Q with three people
+            Queue<Person> peopleQ = new Queue<Person>();
+            peopleQ.Enqueue(new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
+            peopleQ.Enqueue(new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 });
+            peopleQ.Enqueue(new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
+
+            // Peek at first person in Q
+            Console.WriteLine("{0} is first in line!", peopleQ.Peek().FirstName);
+
+            // Remove each person from Q
+            GetCoffee(peopleQ.Dequeue());
+            GetCoffee(peopleQ.Dequeue());
+            GetCoffee(peopleQ.Dequeue());
+
+            // Try to DQ when Q is empty
+            try
+            {
+                GetCoffee(peopleQ.Dequeue());
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Console.WriteLine("\nError! {0}", ioe.Message);
+            }
+        }
+        static void GetCoffee(Person p)
+        {
+            Console.WriteLine("{0} got coffee!", p.FirstName);
+        }
+        static void UseGenericStack()
+        {
+            Stack<Person> stackOfPeople = new Stack<Person>();
+            stackOfPeople.Push(new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
+            stackOfPeople.Push(new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 });
+            stackOfPeople.Push(new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
+
+            // look at the TOP item (last in), POP it, then look again
+            Console.WriteLine("First person is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+            Console.WriteLine("\nFirst person is now: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+            Console.WriteLine("\nFirst person item is now: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+
+            try
+            {
+                Console.WriteLine("\n\nFirst person is: {0}", stackOfPeople.Peek());
+                Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Console.WriteLine("\nError! {0}", ioe.Message);
+            }
+        }
+        static void UseGenericList()
+        {
+            // make a list of Person objects, filled with collection/object init syntax
+            List<Person> people = new List<Person>()
+            {
+                new Person {FirstName = "Homer", LastName = "Simpson", Age=47 },
+                new Person {FirstName = "Marge", LastName = "Simpson", Age=45},
+                new Person {FirstName = "Lisa", LastName = "Simplson", Age=9},
+                new Person {FirstName = "Bart", LastName = "Simpson", Age=8}
+            };
+            // print out num items in List
+            Console.WriteLine("Items in list: {0}", people.Count);
+            // Enumerate over List
+            foreach(Person p in people)
+            {
+                Console.WriteLine(p);
+            }
+            // insert a new person
+            Console.WriteLine("\n=> Inserting new person.");
+            people.Insert(2, new Person { FirstName = "Maggie", LastName = "Simpson", Age = 2 });
+            Console.WriteLine("Items in list: {0}", people.Count);
+            // copy data into a new array
+            Person[] arrayOfPeople = people.ToArray();
+            foreach(Person p in arrayOfPeople)
+            {
+                Console.WriteLine("First Names: {0}", p.FirstName);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Use Generic List *****\n");
+            UseGenericList();
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("***** Use Generic Stack *****\n");
+            UseGenericStack();
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("***** Use Generic Queue *****\n");
+            UseGenericQueue();
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("***** Use SortedSet *****\n");
+            UseSortedSet();
+
+
+            Console.Write("\n\nPress <Enter> to exit. . .");
+            _ = Console.ReadLine();
+        }
+    }
+}
---
+++ FunWithObservableCollections/FunWithObservableCollections/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace FunWithObservableCollections
+{
+    class Program
+    {
+        public enum NotifyCollectionChangedAction
+        {
+            Add=0,
+            Remove=1,
+            Replace=2,
+            Move=3,
+            Reset=4,
+        }
+        static void Main(string[] args)
+        {
+            // make a collection to observe and add a few Person objects
+            ObservableCollection<Person> people = new ObservableCollection<Person>()
+            {
+                new Person{FirstName = "Peter", LastName = "Murphy", Age = 52 },
+                new Person{FirstName = "Kevin", LastName = "Key", Age = 48 },
+            };
+            // wire up the CollectionChanged event
+            people.CollectionChanged += people_CollectionChanged;
+
+            // add an item
+            people.Add(new Person { FirstName = "Andy", LastName = "Anderson", Age = 32 });
+
+            // display list as it is now
+            Console.WriteLine("\nHere is the list of people as it now stands:");
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+            // remove an item: custom-developed by me
+            Person rp = FindMatch(people);
+            people.Remove(rp); // HERE the CollectionChanged event should fire for "Remove"
+
+            // show what remains in the collection
+            Console.WriteLine("\nRemaining people in collection:");
+            foreach(Person p in people)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+            Console.WriteLine("\n\nPress <Enter> to exit. . .");
+            Console.ReadLine();
+        }
+        private static Person FindMatch(ObservableCollection<Person> people)
+        { // custom developed by me because I do not know what I am doing with Generics yet
+            // seems like there should be a better way
+            int index;
+            foreach (Person p in people)
+            {
+                Console.WriteLine();
+                index = people.IndexOf(p);
+                Console.WriteLine($"Index: {index}; Person p: {p.ToString()}.");
+                if (people[index].FirstName == "Kevin")
+                {
+                    return people[index];
+                }
+            }
+            return null;
+        }
+        static void Display(Person p)
+        {
+            Console.WriteLine(p.ToString());
+            //Console.WriteLine(p.GetType());
+        }
+        static void people_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            //what was the action that caused the event?
+            Console.WriteLine("\n=> Action for this event: {0}", e.Action);
+
+            //something was removed
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                Console.WriteLine("\n==>> Here are the OLD items:");
+                foreach (Person p in e.OldItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+                Console.WriteLine();
+            }
+
+            //something was added
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                //now show the NEW items that were inserted
+                Console.WriteLine("\n==>>Here are the NEW items:");
+                foreach(Person p in e.NewItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+            }
+        }
+    }
+}
