bec4333b69d93f1e7d24df172ff631cc4021ee8e
Jon Rumsey <jon.rumsey@gmail.com>
2019-06-18 18:12:29 -0700

    Closed CustomInterface now Interface method clashing fun.

---
+++ CustomInterface/CustomInterface/PointyTestClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterface
+{
+    class PointyTestClass : IPointy
+    {
+        // have Visual Studio auto-generate this code once the Interface reference is there...
+        public byte Points => throw new NotImplementedException();
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public byte GetNumberOfPoints()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ InterfaceNameClash/InterfaceNameClash/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Interface Name Clashes *****");
+            // All of these invocations call the same Draw() method!
+
+            Octogon oct = new Octogon();
+            IDrawToForm itfForm = (IDrawToForm)oct;
+            itfForm.Draw();
+
+            IDrawToPrinter itfPrinter = (IDrawToPrinter)oct;
+            itfPrinter.Draw();
+
+            IDrawToMemory itfMemory = (IDrawToMemory)oct;
+            itfMemory.Draw();
+
+
+            Console.ReadLine();
+        }
+    }
+    public interface IDrawToForm
+    {
+        void Draw();
+    }
+    public interface IDrawToMemory
+    {
+        void Draw();
+    }
+    public interface IDrawToPrinter
+    {
+        void Draw();
+    }
+    class Octogon : IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+        public void Draw()
+        {
+            // Shared drawing logic
+            Console.WriteLine("Drawing the Octagon...");
+        }
+    }
+}
