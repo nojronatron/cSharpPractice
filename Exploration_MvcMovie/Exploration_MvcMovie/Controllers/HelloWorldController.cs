35b24769e196e05e10b703238872f6ffd1e6db92
Jon Rumsey <jon.rumsey@gmail.com>
2019-06-28 14:54:46 -0700

    Using the ViewBag object to pass data via Views.

--- Exploration_MvcMovie/Exploration_MvcMovie/Controllers/HelloWorldController.cs
+++ Exploration_MvcMovie/Exploration_MvcMovie/Controllers/HelloWorldController.cs
@@ -8,21 +8,19 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/ 
 
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
-        }
-        public string Jonru()
-        {
-            return "Jon's custom page";
+            ViewBag.Message = "Hello, " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
         }
     }
 }
