97624f98e08849bd004aae57d3f5e4727177c770
Jon Rumsey <jon.rumsey@gmail.com>
2019-07-11 15:01:43 -0700

    Exploring Exponent algo and Prime algo.

---
+++ PerformingExponentiation/PerformingExponentiation/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PerformingExponentiation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw1 = new Stopwatch();
+            double a = 2; // number
+            double p = 32; // power
+            Console.WriteLine("***** Fun with Exponentiation *****");
+            Console.WriteLine($"Inputs: {a}^{p}\n");
+
+            // use dotnet POW()
+            sw1.Start();
+            double pPow = Math.Pow(a, p);
+            sw1.Stop();
+            Console.WriteLine($"Using DotNet Math.Pow(a, p)...");
+            Console.WriteLine($"Result: {pPow:N}");
+            Console.WriteLine($"Elapsed Ticks: {sw1.ElapsedTicks}\n");
+
+            // use multiplication to find powers
+            sw1.Start();
+            double mp = MultiplicativePowers(a, p);
+            sw1.Stop();
+            Console.WriteLine($"MultiplicativePowers (Start with {a}, then multiply it with itself {p - 1} more times)...");
+            Console.WriteLine($"Result: {mp:N}");
+            Console.WriteLine($"Elapsed Ticks: {sw1.ElapsedTicks}\n");
+            sw1.Reset();
+
+            // use Exponentiation (Book pg.35)
+            sw1.Start();
+            double cp = CalculatePowers(a, p);
+            sw1.Stop();
+            Console.WriteLine($"Calculated Exponentiation...");
+            Console.WriteLine($"Result: {cp:N}");
+            Console.WriteLine($"Elapsed Ticks: {sw1.ElapsedTicks}\n");
+
+            // check all answers
+            if (pPow.CompareTo(mp) == 0)
+            {
+                if (pPow.CompareTo(cp) == 0)
+                {
+                    Console.WriteLine($"\nAll results are the same!");
+                }
+                else if (mp.CompareTo(cp) != 0)
+                {
+                    Console.WriteLine($"Muliplicative Powers {mp} and Calculated Explonentiation {cp} are different.");
+                }
+                else
+                {
+                    Console.WriteLine($"DotNet Math {pPow} and Calculated Exponentiation {cp} are different.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"DotNet math {pPow} and MultiplicativePowers {mp} are different.");
+            }
+
+            Console.WriteLine("\nPress <Enter> to exit. . .");
+            Console.ReadLine();
+        }
+        static double MultiplicativePowers(double A, double P)
+        {
+            if (P == 0)
+            {
+                return 1;
+            }
+            double result = A;
+            for (int i = 2; i <= P; i++)
+            {
+                result *= A;
+            }
+            return result;
+        }
+        static double CalculatePowers(double A, double P)
+        {
+            if (P < 0)
+            {
+                return -1; // standard error output alternative to throwing an exception
+            }
+            double N = 0;
+            double tracker = 1;
+            // calculate A, A^2, A^4, A^8... until A^N where N + 1 > P
+            while (N + 1 <= P)
+            {
+                N++;
+                tracker = Math.Pow(A, N);
+            }
+            return tracker;
+        }
+    }
+}
---
+++ WorkingWithPrimeNumbers/WorkingWithPrimeNumbers/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace WorkingWithPrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw1 = new Stopwatch();
+            const double A = 1717171717;
+            // Prime Numbers are counting numbers that are greater than 1, 
+            //  and can only be divided evenly by itself and the number 1
+            // Composite Numbers are counting numbers that are greater than 1
+            //  and are NOT Prime
+            Console.WriteLine("***** Fun with Prime Number Alogorithms *****");
+            Console.WriteLine($"\nInput number: {A}\n");
+
+            Console.WriteLine(" => FindPrimeFactors_Alpha() <=");
+            Display(FindPrimeFactors_Alpha(A));
+            sw1.Start();
+            FindPrimeFactors_Alpha(A);
+            sw1.Stop();
+            Console.WriteLine($"Elapsed Ticks: {sw1.ElapsedTicks}\n");
+
+            sw1.Reset();
+            Console.WriteLine();
+            Console.WriteLine(" => FindPrimeFactors_Bravo() <=");
+            Display(FindPrimeFactors_Bravo(A));
+            sw1.Start();
+            FindPrimeFactors_Bravo(A);
+            sw1.Stop();
+            Console.WriteLine($"Elapsed Ticks: {sw1.ElapsedTicks}\n");
+
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to exit. . .");
+            Console.ReadLine();
+        }
+        static List<double> FindPrimeFactors_Bravo(double number)
+        {
+            // suggested algorithm improvements made here
+            // 1) dividing by any other even number than 2 is not necessary,
+            //  because 2 is the base even Composite number.
+            // 2) check factors only up to the sqrt() of the number
+            //  if n=p*q either p or q must be less than or equal to sqrt(n)
+            //  once the smaller factor is found, n/small_factor = larger_factor.
+            // 3) every time you divide the number by a factor you can update
+            //  the upper bound on the possible factors that you need to check.
+            List<double> factors = new List<double>();
+            // pull out factors of 2
+            while (number % 2 == 0)
+            {
+                factors.Add(2);
+                number = number / 2;
+            } // end while
+            // look for odd factors
+            int i = 3;
+            double max_factor = Math.Sqrt(number);
+            while (i <= max_factor)
+            {
+                // pull out factors of i
+                while (number % i == 0)
+                {
+                    // i is a factor so add it to the list
+                    factors.Add(i);
+                    // divide number by i
+                    number = number / i;
+                    // set new upper bound
+                    max_factor = Math.Sqrt(number);
+                } // end while
+                // check next possible odd factor
+                i = i + 2;
+            } // end while
+            // if there is anything left of number then it is a factor too
+            if (number > 1)
+            {
+                factors.Add(number);
+            }
+            return factors;
+        }
+        static List<double> FindPrimeFactors_Alpha(double number)
+        {
+            List<double> factors = new List<double>();
+            for (int i = 2; i < number; i++)
+            {
+                // pull out factors of i
+                while (number % i == 0)
+                {
+                    // i is a factor so add it to the list
+                    factors.Add(i);
+                    // divide by i
+                    number = number / i;
+                }
+                // end While... check next possible factor
+            }
+            if (number > 1)
+            {
+                factors.Add(number);
+            }
+            return factors; // list<int> of factors
+        }
+        static void Display(List<double> factors)
+        {
+            if (factors.Count < 1)
+            {
+                throw new ArgumentNullException("The list was empty!");
+            }
+            Console.WriteLine("\nList of Factors");
+            Console.WriteLine("~~~~~~~~~~~~~~~");
+            Console.WriteLine("Index\tValue");
+            for (int index = 0; index < factors.Count; index++)
+            {
+                Console.WriteLine($"{index}\t{factors[index]}");
+            }
+            Console.WriteLine("~~~~~~~~~~~~~~~\n");
+        }
+    }
+}
