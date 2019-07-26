b3587d250a14a4f289381fac661c00d054a343d9
Jon Rumsey <jon.rumsey@gmail.com>
2019-07-25 20:59:35 -0700

    In-class work and exercises using HashTables.

---
+++ HashTables_InClassExercise/HashTables_InClassExercise/Book.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables_InClassExercise
+{
+    public class Book
+    {
+        private string _title;
+        private string _author;
+        private string _publisher;
+        private string _isbn;
+        private decimal _price;
+
+        public Book(string title, string author, string publisher, string isbn, decimal price)
+        {
+            this._author = author;
+            this._title = title;
+            this._price = price;
+            this._isbn = isbn;
+            this._publisher = publisher;
+        }
+
+        public string Title
+        { get { return this._title; } }
+
+        public string Author
+        { get { return this._author; } }
+
+        public string Publisher
+        { get { return this._publisher; } }
+
+        public string ISBN
+        { get { return this._isbn; } }
+
+        public decimal Price
+        { get { return this._price; } }
+
+        public override string ToString()
+        {
+            return $"{_title,-20}{_publisher,-20}{_author,-20}{_isbn,-20}{_price:c}";
+        }
+    }
+
+}
--- HashTables_InClassExercise/HashTables_InClassExercise/Program.cs
+++ HashTables_InClassExercise/HashTables_InClassExercise/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace HashTables_InClassExercise
 {
@@ -8,27 +9,66 @@ namespace HashTables_InClassExercise
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun With Hashtables *****");
-            MyHashtable t1 = new MyHashtable();
-            t1.Add(0, "John, 425-555-1212");
-            t1.Add(10, "Melissa, 206-555-1213");
-            t1.Add(15, "Kris, 509-555-1214");
-            t1.Add(19, "Sarah, 425-555-1215");
-            t1.Add(5, "Aaron, 206-555-1216");
+            //MyHashtable t1 = new MyHashtable();
+            //t1.Add(0, "John, 425-555-1212");
+            //t1.Add(10, "Melissa, 206-555-1213");
+            //t1.Add(15, "Kris, 509-555-1214");
+            //t1.Add(19, "Sarah, 425-555-1215");
+            //t1.Add(5, "Aaron, 206-555-1216");
 
-            DisplayHashtable(t1);
-            Console.Write("\nEnter a key to search for: ");
-            int key = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Search result: {SearchHashtable(t1, key)}.");
+            //DisplayHashtable(t1);
+            //Console.Write("\nEnter a key to search for: ");
+            //int key = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Search result: {SearchHashtable(t1, key)}.");
 
-            Console.Write("\nEnter a key to search for: ");
-            key = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Search result: {SearchHashtable(t1, key)}.");
+            //Console.Write("\nEnter a key to search for: ");
+            //key = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Search result: {SearchHashtable(t1, key)}.");
 
-            Console.Write("\nEnter a key to remove: ");
-            key = int.Parse(Console.ReadLine());
-            RemoveItemFromHashtable(t1, key);
+            //Console.Write("\nEnter a key to remove: ");
+            //key = int.Parse(Console.ReadLine());
+            //RemoveItemFromHashtable(t1, key);
 
-            DisplayHashtable(t1);
+            //DisplayHashtable(t1);
+
+            // ==========================
+            // in dotnet, Dictionary is a generic and is "the new Hashtable"
+            //      is: a Key-Value pair
+
+            Book b1 = new Book(
+                            "Computer Science Illuminated",
+                            "Nell Dale",
+                            "Jones & Bartlett",
+                             "9781449672843",
+                             50.89m);
+            Book b2 = new Book(
+                "Nine Algorithms That Changed the Future",
+                "John MacCormick",
+                 "Princeton University Press",
+                 "9780691147147",
+                8.99m);
+
+            Book b3 = new Book(
+                "Python Programming 2nd Edition",
+                "John Zelle",
+                "Franklin, Beedle & Associates Inc",
+                "9781590282410",
+                42.75m);
+
+            Dictionary<string, Book> bookDict = new Dictionary<string, Book>(); // <Key, Value> as Types
+            bookDict.Add(b1.ISBN, b1); // uses ISBN as the key and the entire b1 object as the value
+                                       // alternatively, the key could be manually generated
+            bookDict.Add(b2.ISBN, b2);
+            bookDict.Add(b3.ISBN, b3);
+            
+            // apply a foreach to a dictionary
+            foreach (KeyValuePair<string, Book> kvp in bookDict)
+            {
+                Book book = kvp.Value;
+                Console.WriteLine(book);    // default action is the overridden ToString()
+            }
+
+            // ==========================
 
             Console.Write("Press <Enter> key to exit. . .");
             Console.ReadLine();
