68d2fa6634c354641df23120d3f31d18e7e7e58d
Jon Rumsey <jon.rumsey@gmail.com>
2019-08-25 18:12:29 -0700

    Linq on my own! Also StringToIntATIO (unverified).

---
+++ FilterEvenNumbersFromListUsingLinq/FilterEvenNumbersFromListUsingLinq/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilterEvenNumbersFromListUsingLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Filter out the first 3 even numbers from the list using LINQ
+            List<int> list = new List<int>(10)
+            {
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+            };
+
+            List<int> filteredList = iterFilter(list);
+            Console.WriteLine($"*** Filtered List using List<int> iterators ***");
+            DisplayList(filteredList);
+
+            Console.WriteLine($"*** Filtered List using Linq Queries ***");
+            List<int> linqFilteredList = linqFilter(list);
+            DisplayList(linqFilteredList);
+
+            Console.WriteLine("\n\nPress <Enter> to exit. . .");
+            Console.ReadLine();
+        }
+        static List<int> linqFilter(List<int> list)
+        {
+            List<int> evens = (from num in list
+                            where (num % 2 == 0)
+                            select num).ToList();   // 0, 2, 4, 6, 8
+            evens.RemoveRange(0, 3);                // 6, 8
+            List<int> odds = (from num in list
+                            where (num % 2 != 0)
+                            select num).ToList();   // 1, 3, 5, 7, 9
+            List<int> concatList = evens.Concat(odds).ToList(); // 6, 8, 1, 3, 5, 7, 9
+            return (from num in concatList
+                    orderby num ascending
+                    select num).ToList();           // 1, 3, 5, 6, 7, 8, 9
+        }
+        static void DisplayList(List<int> list)
+        {
+            foreach (int j in list)
+            {
+                Console.Write($"{j}\t");
+            }
+            Console.WriteLine();
+        }
+        static List<int> iterFilter(List<int> list)
+        {
+            List<int> filteredList = new List<int>();
+            int counter = 0;
+            foreach (int i in list)
+            {
+                int result = i % 2;
+                if (i >= 2)
+                {
+                    if (result == 0)
+                    {
+                        counter++;
+                        if (counter > 3)
+                        {
+                            filteredList.Add(list[i]); // 8,
+                        }
+                    } // dropped: 2, 4, 6
+                    else
+                    {
+                        filteredList.Add(list[i]); // 9,
+                    }
+                }
+                else
+                {
+                    filteredList.Add(list[i]); // 0, 1, 3, 5, 7
+                }
+            }
+            return filteredList;
+        }
+    }
+}
--- StringToIntATOI/StringToIntATOI/Program.cs
+++ StringToIntATOI/StringToIntATOI/Program.cs
@@ -52,13 +52,16 @@ namespace StringToIntATOI
                 Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
              Thefore INT_MIN (−231) is returned.
              */
-            const int LISTSIZE = 15;
+            const int LISTSIZE = 18;
             List<string> exampleInputs = new List<string>(LISTSIZE);
+            exampleInputs.Add("    -42");
+            exampleInputs.Add(" +0 123");
+            exampleInputs.Add("1");
+            exampleInputs.Add("20000000000000000000");
             exampleInputs.Add("0");
             exampleInputs.Add("    ");
             exampleInputs.Add("   abc-777 xyz  ");
             exampleInputs.Add("42");
-            exampleInputs.Add("    -42");
             exampleInputs.Add("4193 with words");
             exampleInputs.Add("words and 987");
             exampleInputs.Add("-91283472332");
@@ -66,16 +69,19 @@ namespace StringToIntATOI
             exampleInputs.Add("  one  ");
             exampleInputs.Add("+1");
             exampleInputs.Add("+-1");
-            exampleInputs.Add("+-2");
+            exampleInputs.Add("-+2");
             exampleInputs.Add("-");
             exampleInputs.Add("+");
 
             List<int> exampleOutputs = new List<int>(LISTSIZE);
+            exampleOutputs.Add(-42);
+            exampleOutputs.Add(0);
+            exampleOutputs.Add(1);
+            exampleOutputs.Add(2147483647);    // aka INT_MAX
             exampleOutputs.Add(0);
             exampleOutputs.Add(0);
             exampleOutputs.Add(0);
             exampleOutputs.Add(42);
-            exampleOutputs.Add(-42);
             exampleOutputs.Add(4193);
             exampleOutputs.Add(0);
             exampleOutputs.Add(-2147483648);    // aka INT_MIN
@@ -102,92 +108,199 @@ namespace StringToIntATOI
                 string s = exampleInputs[n];
                 //Thread.Sleep(1000);
                 //sw.Start();
-                int result = doATOI(s);
+                string test1 = DiscardLeadingWhitespace(s);
+                string test2 = GetDigitsFollowingPolaritySign(s, "-");
+                string test3 = DiscardTrailingAlphaCharacters(s);
+                int result = DoATOI(s);
                 //sw.Stop();
-                Console.WriteLine($"-->> Input: {s,16}  Expected: {exampleOutputs[n],11}" +
-                    $"  Actual: {result,11}  Type: {(result.GetType().Name),6}"); //  Ticks: {sw.ElapsedTicks}");
+                Console.WriteLine($"-->> Input: {s,20}  Expected: {exampleOutputs[n],11}" +
+                    $"  Actual: {result,11}");//  Ticks: {sw.ElapsedTicks}");
                 //sw.Reset();
             }
 
             Console.Write("\n\nPress <Enter> to Exit. . .");
             Console.ReadLine();
         }
-        static int doATOI(string arg)
+
+
+        static string DiscardLeadingWhitespace(string s)
         {
-            int result = 0;
-            string strResult = "";
-            try
-            {
-                arg = arg.Replace(" ", ""); // 1. discard whitespace characters to the 1st non-whitespace character
-                if (arg.Length < 1)
-                { // 4. if nothing is left after removing whitespace no conversion is performed so return 0
-                    strResult = "0";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            { // discard leading whitespace chars
+                if (!char.IsWhiteSpace(s[i]))
+                { // retain non-whitespace chars
+                    sb.Append(s[i]);
                 }
-                for (int index = 0; index < arg.Length; index++)
+                else
                 {
-                    if (arg.Substring(index, 1) == "-" && index+1 < arg.Length)
+                    break;
+                }
+            }
+            return sb.ToString();
+        }
+        static string GetDigitsFollowingPolaritySign(string s, string polaritySign)
+        {
+            int polaritySignIndex = s.IndexOf(polaritySign);
+            StringBuilder sb = new StringBuilder();
+            if (polaritySignIndex > -1)
+            { // s contains a polarity sign
+                if (char.IsDigit(s[polaritySignIndex + 1]))
+                { // polarity sign is followed by at least one digit
+                    if (polaritySign == "-")
                     {
-                        if (int.TryParse(arg.Substring(index + 1, 1), out int q))
-                        {// 2. look for minus sign while recording digits
-                            strResult += "-";
-                            _ = q;  // but ignore if next character is non-int
-                        }
+                        sb.Append("-");
                     }
-                    else if (arg.Substring(index, 1) == "+" & index+1 < arg.Length)
-                    {
-                        if (int.TryParse(arg.Substring(index + 1, 1), out int p))
-                        { // found a rogue plus sign in front of an integer ignore it and carry on
-                            _ = p;
+                    for (int i = polaritySignIndex + 1; i < s.Length; i++)
+                    { // cycle through string to find remaining digits
+                        if (char.IsDigit(s[i]))
+                        { // add next digit to the stringbuilder
+                            sb.Append(s[i]);
                         }
                         else
-                        { // found a rogue plus sign in front of a non-integer so break out and return a result
-                            strResult = "0";
+                        { // stop adding to stringbuilder when digits no longer found
                             break;
                         }
                     }
-                    else if (int.TryParse(arg.Substring(index, 1), out int r))
-                    {// 3. find only int32 type characters
-                        strResult += $"{r}";   // default behavior is ToString()
-                    }
-                    else if (strResult.Length > 0)
-                    {   // 3. trailing-edge non-numeric characters have been found break out of this to return the result
-                        break;
-                    }
-                    else
-                    {   // 4. first sequence of non-whitespace chars in str is not valid integral number
-                        // 5. if no valid conversion could be performed a 0 is returned
-                        strResult = "0";
-                        break;
+                }
+            }
+            return sb.ToString();
+        }
+        static string DiscardTrailingAlphaCharacters(string s)
+        {
+            int index = 0;
+            StringBuilder sb = new StringBuilder(s);
+            if (s[index].Equals("-"))
+            {
+                index = 1;
+            }
+            for (int i = index; i < s.Length; i++)
+            { // discard trailing alpha-characters
+                if (char.IsLetter(sb[i]))
+                {
+                    sb.Remove(i, sb.Length - i);
+                }
+            }
+            return sb.ToString();
+            sb.Append()
+        }
+
+
+        static int DoATOI(string arg)
+        {
+            int result = 0; // will contain return value
+            int intSign = 1; // assume positive integer and -1 is negative integer
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                if (arg.Length < 1)
+                { // if empty string return 0
+                    return 0;
+                }
+                if (arg.Length == 1)
+                {
+                    if (char.IsDigit(arg, 0))
+                    { // the only character is a digit so return it
+                        return int.Parse(arg);
                     }
                 }
-                result = int.Parse(strResult);
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("FormatException was caught.");
-                result = -1;
+                arg = DiscardLeadingWhitespace(arg);
+                // arg = sb.ToString();
+                // sb.Clear();
+
+                arg = DiscardTrailingAlphaCharacters(arg);
+
+
+                if (sb.Length < 1)
+                { // input was all whitespace
+                    return 0;
+                }
+                arg = sb.ToString(); // re-sync sb to arg
+                sb.Clear();
+
+                int negativeSign = arg.IndexOf("-");
+                int positiveSign = arg.IndexOf("+");
+                StringBuilder sbPos = new StringBuilder();
+                StringBuilder sbNeg = new StringBuilder();
+
+                if (negativeSign > -1)
+                { // arg contains a negative sign
+                    if (char.IsDigit(arg[negativeSign + 1]))
+                    { // negative sign is followed by at least one digit
+                      // sbNeg.Append("-");
+                        for (int i = negativeSign + 1; i < arg.Length; i++)
+                        { // cycle through string to find remaining digits
+                            if (char.IsDigit(arg[i]))
+                            { // add next digit to the stringbuilder
+                                sbNeg.Append(arg[i]);
+                            }
+                            else
+                            { // set intSign to negative and stop adding to stringbuilder
+                                intSign = -1;
+                                break;
+                            }
+                        }
+                    }
+                    if (sbNeg.Length > sbPos.Length)
+                    {
+                        sb = sbNeg;
+                    }
+                }
+                else if (positiveSign > -1)
+                { // arg contains a positive sign
+                    if (char.IsDigit(arg[positiveSign + 1]))
+                    { // positive sign is followed by at least one digit
+                        for (int i = positiveSign + 1; i < arg.Length; i++)
+                        { // cycle through string to find remaining digits
+                            if (char.IsDigit(arg[i]))
+                            { // add next digit to the stringbuilder
+                                sbPos.Append(arg[i]);
+                            }
+                            else
+                            { // set intSign to positive and stop adding to stringbuilder
+                                intSign = 1;
+                                break;
+                            }
+                        }
+                    }
+                    if(sbPos.Length > sbNeg.Length)
+                    {
+                        sb = sbPos;
+                    }
+                }
+                if (sb.Length > 0)
+                {
+                    result = int.Parse(sb.ToString());
+                }
+                else
+                {
+                    result = int.Parse(arg);
+                }
             }
             catch (OverflowException)
-            {   // 6. change the result to INT_MIN or INT_MAX
-                if (float.TryParse(arg, out float r))
+            {
+                if (intSign == -1)
                 {
-                    if (r < 0)
-                    {
-                        result = int.MinValue;
-                    }
-                    else
-                    {
-                        result = int.MaxValue;
-                    }
+                    result = int.MinValue;
                 }
+                else
+                {
+                    result = int.MaxValue;
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                result = 0;
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine($">>DEBUG>> Arg: {arg} sb: {sb}\n\n{fe}");
+                Console.ReadLine();
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Something unexpected happened.\n{ex.Message}\n");
-                if (ex.InnerException != null)
-                {
-                    Console.WriteLine($"Inner Exception: {ex.InnerException.Message}");
-                }
+                Console.WriteLine($"An uncaught exception occurred. Arg: {arg} sb: {sb}\n\n{ex}");
+                Console.ReadLine();
             }
             return result;
         }
