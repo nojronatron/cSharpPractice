d7393856000202c8a9f79ac56b3da124d59ffda1
Jon Rumsey <jon.rumsey@gmail.com>
2019-10-25 12:46:42 -0700

    First few pages of C# Pro Chapter 7 Structured Exception Handling.

---
+++ SimpleException/SimpleException/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Car
+    {
+        //  constant for maximum speed
+        public const int MAXSPEED = 100;
+
+        //  car props
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        //  is the car still operational?
+        private bool carIsDead;
+
+        //  a car has-a radio
+        private Radio theMusicBox = new Radio();
+
+        // constructors
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            //  delegate request to inner object
+            theMusicBox.TurnOn(state);
+        }
+
+        //  see if car has overheated
+        public void Accelerate(int delta)
+        {   //  create and configure an exception if the user attempts to speed above MAXSPEED and send that exception back to the caller
+            if (carIsDead)
+            {
+                Console.WriteLine($"{PetName} is out of order...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MAXSPEED)
+                {
+                    //  Console.WriteLine($"{PetName} has overheated!");
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+
+                    //  use the throw keyword to raise an exception
+                    throw new Exception($"{PetName} has overheated!");  //   the executable will write the exception text to the default output/console
+                }
+                else
+                {
+                    Console.WriteLine($"=> CurrentSpeed = {CurrentSpeed}");
+                }
+            }
+        }
+    }
+}
---
+++ SimpleException/SimpleException/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  get the car to overheat
+            Console.WriteLine("***** Simple Exception Example *****");
+            Console.WriteLine("=> Creating a car and flooring the accellerator!");
+
+            Car myCar = new Car("Zippy", 20);
+            myCar.CrankTunes(true);
+
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    myCar.Accelerate(10);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("\n*** Error! ***");
+                Console.WriteLine($"Method: {ex.TargetSite}");
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine($"Source: {ex.Source}");
+            }   //  The error has been handled, processing continues with the next statements...
+            Console.WriteLine("\n***** Out of exception logic *****");
+            Console.ReadLine();
+        }
+    }
+}
