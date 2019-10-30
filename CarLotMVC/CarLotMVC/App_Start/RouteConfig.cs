01ca89f32fad9193c8f10b21b1a02b80cf2eb83e
Jon Rumsey <jon.rumsey@gmail.com>
2019-10-29 21:07:05 -0700

    Chpt 29 ASP.NET MVC

---
+++ CarLotMVC/CarLotMVC/App_Start/RouteConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CarLotMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //  *pathinfo allows any number of additional URL parameters following */Contact/
+            routes.MapRoute(
+                name: "Contact", 
+                url: "Contact/{*pathinfo}", 
+                defaults: new { controller = "Home", action = "Contact" }
+                );
+
+            routes.MapRoute(
+                name: "About",
+                url: "About/{*pathinfo}",
+                defaults: new { controller = "Home", action = "About" }
+                );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ CarLotMVC/CarLotMVC/Controllers/InventoryController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutoLotDAL.EF;
+using AutoLotDAL.Models;
+using AutoLotDAL.Repos;
+using System.Data.Entity.Infrastructure;
+
+namespace CarLotMVC.Controllers
+{
+    public class InventoryController : Controller
+    {
+        //  private AutoLotEntities db = new AutoLotEntities();
+        private readonly InventoryRepo _repo = new InventoryRepo();
+
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            // return View(_repo.GetAll());
+            return View(_repo.GetAll());
+        }
+
+        // GET: Inventory/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = _repo.GetOne(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventory/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inventory/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Make,Color,PetName")] Inventory inventory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(inventory);
+            }
+            try
+            {
+                _repo.Add(inventory);
+                //db.SaveChanges();
+                //return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $@"Unable to create record: {ex.Message}");
+                return View(inventory);
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Inventory/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = _repo.GetOne(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventory/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Make,Color,PetName,Timestamp")] Inventory inventory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(inventory);
+                //db.Entry(inventory).State = EntityState.Modified;
+                //db.SaveChanges();
+                //return RedirectToAction("Index");
+            }
+            try
+            {
+                _repo.Save(inventory);
+            }
+            catch (DbUpdateConcurrencyException dbucex)
+            {
+                ModelState.AddModelError(string.Empty,
+                    $@"Unable to save the record. Another user has updated it. {dbucex.Message}");
+                return View(inventory);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty,
+                    $@"Unable to save the record. {ex.Message}");
+                return View(inventory);
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Inventory/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = _repo.GetOne(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventory/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete([Bind(Include="Id,Timestamp")]Inventory inventory)
+        {
+            //Inventory inventory = db.Cars.Find(id);
+            //db.Cars.Remove(inventory);
+            //db.SaveChanges();
+            try
+            {
+                _repo.Delete(inventory);
+            }
+            catch(DbUpdateConcurrencyException dbucex)
+            {
+                ModelState.AddModelError(string.Empty,
+                    $@"Unable to delete record. Another user updated the record. {dbucex.Message}");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty,
+                    $@"Unable to delete record. {ex.Message}");
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //db.Dispose();
+                _repo.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
