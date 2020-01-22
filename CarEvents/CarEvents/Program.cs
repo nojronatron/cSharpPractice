112593a0e5edb95b14f207da46483ea314bace7a
Jon Rumsey <jon.rumsey@gmail.com>
2020-01-21 19:24:00 -0800

    C# Pro 7 Chpt 10 events.

---
+++ CarEvents/CarEvents/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Events *****");
+            Car myCar = new Car("Ted", 100, 10);
+
+            //  register event handlers (method) with the instance's public event by using +=
+            myCar.AboutToBlow += new Car.CarEngineHandler(CarIsAlmostDoomed);
+            myCar.AboutToBlow += new Car.CarEngineHandler(CarAboutToBlow);
+
+            //  attach/register with an event with +=
+            Car.CarEngineHandler dead = new Car.CarEngineHandler(CarExploded);  //  the delegate 'CarEngineHandler'
+            myCar.Exploded += dead; //  registering CarEngineHandler 'dead' with the event
+
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i <= 6; i++)
+            {
+                myCar.Accelerate(20);
+            }
+
+            //  detach from a source of events with -=
+            myCar.Exploded -= dead;
+
+            Console.WriteLine("\n***** Speeding up *****");
+            for (int i=0; i<6; i++)
+            {
+                myCar.Accelerate(20);
+            }
+
+            //  add it back and see what happens
+            myCar.Exploded += dead;
+
+            Console.WriteLine("\n***** Speeding up *****");
+            for (int i = 0; i <= 6; i++)
+            {
+                myCar.Accelerate(20);
+            }
+
+
+            Console.WriteLine("\nPress <Enter> to exit. . .");
+            Console.ReadLine();
+        }
+        //  create static methods for the caller
+        public static void CarAboutToBlow(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        public static void CarIsAlmostDoomed(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        public static void CarExploded(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
