e0c95585bf19e3dffce0d0b731813be66a22dae2
Jon Rumsey <jon.rumsey@gmail.com>
2020-01-28 14:13:49 -0800

    Intro Lambda with multiple params.

---
+++ LambdaExpressionMultipleParams/LambdaExpressionMultipleParams/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressionMultipleParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Register with delegate as a lambda expression
+            SimpleMath m = new SimpleMath();
+            m.SetMathHandler((msg, result) =>
+            {
+                Console.WriteLine($"Message: { msg }\tResult: { result }");
+            });
+            //  this will execute the Lambda expression
+            m.Add(10, 10);
+
+
+
+            Console.Write("\n\nPress <Enter> to Exit. . .");
+            Console.ReadLine();
+        }
+    }
+    public class SimpleMath
+    {
+        public delegate void MathMessage(string msg, int result);
+        private MathMessage mmDelegate;
+
+        public void SetMathHandler(MathMessage target)
+        {
+            mmDelegate = target;
+        }
+
+        public void Add(int x, int y)
+        {
+            mmDelegate?.Invoke("Adding has completed!", x + y);
+        }
+    }
+}
