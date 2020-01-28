3e1b26931f713c365b35390af937cf6f5c0b69ee
Jon Rumsey <jon.rumsey@gmail.com>
2020-01-28 11:13:18 -0800

    Pro C#7 Chpt 10 bookwork.

---
+++ CarEvents_AnonymousMethod/CarEvents_AnonymousMethod/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEvents_AnonymousMethod
+{
+    public class Car
+    {
+        //  this car can send these events
+        public event EventHandler<CarEventArgs> Exploded;
+        public event EventHandler<CarEventArgs> AboutToBlow;
+
+        //  internal state data
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+        //  is the car alive or dead
+        private bool carIsDead;
+        //  ctors
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+            carIsDead = false;
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                //  if this car is dead send dead message
+                Exploded?.Invoke(this, new CarEventArgs("Sorry, this car is dead..."));
+            }
+            //  is this car almost dead
+            else if (10 == MaxSpeed - CurrentSpeed)
+            {
+                AboutToBlow?.Invoke(this, new CarEventArgs("Careful buddy! Gonna blow!"));
+            }
+            else if (CurrentSpeed >= MaxSpeed)
+            {
+                carIsDead = true;
+            }
+            else
+            {
+                Console.WriteLine($"Current Speed = { CurrentSpeed }.");
+            }
+            CurrentSpeed += delta;
+        }
+    }
+}
---
+++ CarEvents_AnonymousMethod/CarEvents_AnonymousMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEvents_AnonymousMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Anonymous Methods *****\n");
+            Car c1 = new Car("SlugBug", 100, 10);
+
+            //  register event handlers as anonymous methods
+            c1.AboutToBlow += delegate
+            {
+                Console.WriteLine("Eek! Going too fast!");
+            };
+
+            c1.AboutToBlow += delegate (object sender, CarEventArgs e)
+            {
+                Console.WriteLine($"Message from Car: { e.msg }.");
+            };
+
+            c1.Exploded += delegate (object sender, CarEventArgs e)
+            {
+                Console.WriteLine($"Fatal message from Car: { e.msg }.");
+            };
+
+            //  this will eventually trigger the events
+            for (int i=0; i<7; i++)
+            {
+                c1.Accelerate(20);
+            }
+
+
+
+            Console.Write("\n\nPress <Enter> to exit. . .");
+            Console.ReadLine();
+        }
+    }
+}
