26bfc0c6c8fa36b47283a1c7dfde3a7e40659f5e
Jon Rumsey <jon.rumsey@gmail.com>
2020-01-29 21:15:48 -0800

    In Class Task Creation and Handling in Windows Forms.

---
+++ TasksWithWindowsForms/TasksWithWindowsForms/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace TasksWithWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonStartTasks = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnStartTaskWithTResult = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.btnStartTaskWithTResultNonBlocking = new System.Windows.Forms.Button();
+            this.btnChainingTasks = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(286, 700);
+            this.listBox1.TabIndex = 0;
+            // 
+            // buttonStartTasks
+            // 
+            this.buttonStartTasks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStartTasks.Location = new System.Drawing.Point(12, 732);
+            this.buttonStartTasks.Name = "buttonStartTasks";
+            this.buttonStartTasks.Size = new System.Drawing.Size(583, 45);
+            this.buttonStartTasks.TabIndex = 1;
+            this.buttonStartTasks.Text = "Start Tasks";
+            this.buttonStartTasks.UseVisualStyleBackColor = true;
+            this.buttonStartTasks.Click += new System.EventHandler(this.buttonStartTasks_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 29;
+            this.listBox2.Location = new System.Drawing.Point(305, 13);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(290, 700);
+            this.listBox2.TabIndex = 2;
+            // 
+            // btnStartTaskWithTResult
+            // 
+            this.btnStartTaskWithTResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartTaskWithTResult.Location = new System.Drawing.Point(610, 471);
+            this.btnStartTaskWithTResult.Name = "btnStartTaskWithTResult";
+            this.btnStartTaskWithTResult.Size = new System.Drawing.Size(423, 93);
+            this.btnStartTaskWithTResult.TabIndex = 3;
+            this.btnStartTaskWithTResult.Text = "Start Tasks that Return Value but Blocks the UI Thread";
+            this.btnStartTaskWithTResult.UseVisualStyleBackColor = true;
+            this.btnStartTaskWithTResult.Click += new System.EventHandler(this.btnStartTaskWithTResult_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 29;
+            this.listBox3.Location = new System.Drawing.Point(610, 13);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(432, 410);
+            this.listBox3.TabIndex = 4;
+            // 
+            // btnStartTaskWithTResultNonBlocking
+            // 
+            this.btnStartTaskWithTResultNonBlocking.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartTaskWithTResultNonBlocking.Location = new System.Drawing.Point(610, 583);
+            this.btnStartTaskWithTResultNonBlocking.Name = "btnStartTaskWithTResultNonBlocking";
+            this.btnStartTaskWithTResultNonBlocking.Size = new System.Drawing.Size(423, 84);
+            this.btnStartTaskWithTResultNonBlocking.TabIndex = 5;
+            this.btnStartTaskWithTResultNonBlocking.Text = "Start Tasks that Return Value and Query The Results Non-Blocking";
+            this.btnStartTaskWithTResultNonBlocking.UseVisualStyleBackColor = true;
+            this.btnStartTaskWithTResultNonBlocking.Click += new System.EventHandler(this.btnStartTaskWithTResultNonBlocking_Click);
+            // 
+            // btnChainingTasks
+            // 
+            this.btnChainingTasks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChainingTasks.Location = new System.Drawing.Point(610, 693);
+            this.btnChainingTasks.Name = "btnChainingTasks";
+            this.btnChainingTasks.Size = new System.Drawing.Size(423, 84);
+            this.btnChainingTasks.TabIndex = 6;
+            this.btnChainingTasks.Text = "Start Chained Tasks";
+            this.btnChainingTasks.UseVisualStyleBackColor = true;
+            this.btnChainingTasks.Click += new System.EventHandler(this.btnChainingTasks_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1330, 789);
+            this.Controls.Add(this.btnChainingTasks);
+            this.Controls.Add(this.btnStartTaskWithTResultNonBlocking);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.btnStartTaskWithTResult);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.buttonStartTasks);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonStartTasks;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnStartTaskWithTResult;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button btnStartTaskWithTResultNonBlocking;
+        private System.Windows.Forms.Button btnChainingTasks;
+    }
+}
+
---
+++ TasksWithWindowsForms/TasksWithWindowsForms/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TasksWithWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        private delegate void SafeCallDelegate(string text, ListBox listBox);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStartTasks_Click(object sender, EventArgs e)
+        {
+            //  Task(Action)
+            Task task1 = Task.Factory.StartNew(() =>
+            {
+                Random rand = new Random();
+                for (int i = 1; i <= 200; i++)
+                {
+                    int x = rand.Next();
+                    if (x % 3 == 0)
+                    {
+                        //  if x is divisible by 3 then display it
+                        //  listBox1.Items.Add(x);  //  directly display to a listbox
+                        //  *** this will cause a cross thread operation 
+                        //  replace with thread-safe method
+                        WriteTextSafe(x.ToString(), listBox1);
+                        //  simulate a long process
+                    }
+                    //  create a 2nd task that display divisibles by 7 to listBox2
+                    if (x % 7 == 0)
+                    {
+                        WriteTextSafe(x.ToString(), listBox2);
+                    }
+                    Thread.SpinWait(20000000);  //  hog the cpu for a little bit
+                }
+            });
+        }
+        //  thread safe method
+        private void WriteTextSafe(string text, ListBox listBox)
+        {
+            if (listBox.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(WriteTextSafe);
+                listBox.Invoke(d, new object[] { text, listBox });   //  can use 'this' instead of 'listBox'
+            }
+            else
+            {
+                listBox.Items.Add(text);
+            }
+        }
+
+        private void btnStartTaskWithTResult_Click(object sender, EventArgs e)
+        {
+            //  use Task<TResult> for methods or lambda expressions that return a vlue
+            Task<double> task1 = Task<double>.Factory.StartNew(() =>
+           {
+               //   a msg to indicate this has started
+               WriteTextSafe("task1 has just started, returning average", listBox3);
+               Random rand = new Random();
+               double total = 0;
+               for (int i = 1; i <= 200; i++)
+               {
+                   total += rand.Next();
+                   Thread.SpinWait(2000000);
+               }
+               double avg = total / 200;
+               return avg;  //  Task<double> REQUIRES returning a value of type double
+           });
+            //  to capture the return value from a Task<TResult> a query to the Result
+            //  property is required
+            listBox3.Items.Add("Waiting for results to return from task1...");
+            //double average = task1.Result;  //  this BLOCKS the thread stopping other Tasks
+            //listBox3.Items.Add($"Average value: { average }");
+        }
+
+        private void btnStartTaskWithTResultNonBlocking_Click(object sender, EventArgs e)
+        {
+            Task<double> task1 = Task<double>.Factory.StartNew(() =>
+            {
+                //   a msg to indicate this has started
+                WriteTextSafe("task1 has just started, returning average", listBox3);
+                Random rand = new Random();
+                double total = 0;
+                for (int i = 1; i <= 200; i++)
+                {
+                    total += rand.Next();
+                    Thread.SpinWait(20000000);
+                }
+                double avg = total / 200;
+                return avg;  //  Task<double> REQUIRES returning a value of type double
+            });
+            //  start 2nd Task to Wait and Query the results from Task1
+            //  Task2 should start when Task1 completes its operation
+            Task task2 = task1.ContinueWith(task =>
+            {
+                //  the code here (task2) will start when task1 completes
+                //  the argument, 'task', is a REF to Task task1 (above)
+                //  results from task1 can be accessed via the task REF
+                double average = task.Result;
+                //  display without using xtra method like WriteSafe()
+                //  synch scheduler will take care of safe-thread usage
+                listBox3.Items.Add($"Average value: {average:f5}");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private void btnChainingTasks_Click(object sender, EventArgs e)
+        {
+            //  chaining tasks with CountinueWith
+            Task<int>.Factory.StartNew(() =>
+           {
+               WriteTextSafe("First task has just started", listBox3);
+               Thread.SpinWait(200_000_000);
+               int x = new Random().Next();
+               return x;
+           }).ContinueWith<int>(task =>
+           {
+               WriteTextSafe("Second task has just started", listBox3);
+               Thread.SpinWait(200_000_000);
+               int x = new Random().Next();
+               int value1 = task.Result;
+               return x + value1;
+           }).ContinueWith(task =>
+           {
+               WriteTextSafe("Third task has just started", listBox3);
+               Thread.SpinWait(200_000_000);
+               //  get result return value from previous tasks
+               int result = task.Result;
+               WriteTextSafe($"Overall result: {result}", listBox3);
+           });
+        }
+    }
+}
