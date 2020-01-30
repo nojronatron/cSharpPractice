82acd5bd606233dae44bf423c2c335c93a287e39
Jon Rumsey <jon.rumsey@gmail.com>
2020-01-29 21:16:10 -0800

    In Class handling Task Cancel requests.

---
+++ TaskCancellation/TaskCancellation/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace TaskCancellation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnStartTask = new System.Windows.Forms.Button();
+            this.btnCancelTask = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(369, 294);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnStartTask
+            // 
+            this.btnStartTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartTask.Location = new System.Drawing.Point(12, 381);
+            this.btnStartTask.Name = "btnStartTask";
+            this.btnStartTask.Size = new System.Drawing.Size(175, 57);
+            this.btnStartTask.TabIndex = 1;
+            this.btnStartTask.Text = "Start Task";
+            this.btnStartTask.UseVisualStyleBackColor = true;
+            this.btnStartTask.Click += new System.EventHandler(this.btnStartTask_Click);
+            // 
+            // btnCancelTask
+            // 
+            this.btnCancelTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelTask.Location = new System.Drawing.Point(206, 381);
+            this.btnCancelTask.Name = "btnCancelTask";
+            this.btnCancelTask.Size = new System.Drawing.Size(175, 57);
+            this.btnCancelTask.TabIndex = 2;
+            this.btnCancelTask.Text = "Cancel Task";
+            this.btnCancelTask.UseVisualStyleBackColor = true;
+            this.btnCancelTask.Click += new System.EventHandler(this.btnCancelTask_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 450);
+            this.Controls.Add(this.btnCancelTask);
+            this.Controls.Add(this.btnStartTask);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnStartTask;
+        private System.Windows.Forms.Button btnCancelTask;
+    }
+}
+
---
+++ TaskCancellation/TaskCancellation/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskCancellation
+{
+    public partial class Form1 : Form
+    {
+        //  declare a CancellationTokenSource object to the accessible to both buttons
+        CancellationTokenSource cancellationTokenSource = null; //  just declare for accessibility by buttons; create it later
+
+        private delegate void SafeCallDelegate(string text, ListBox listBox);
+
+        //  thread safe method
+        private void WriteTextSafe(string text, ListBox listBox)
+        {
+            if (listBox.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(WriteTextSafe);
+                listBox.Invoke(d, new object[] { text, listBox });   //  can use 'this' instead of 'listBox'
+            }
+            else
+            {
+                listBox.Items.Add(text);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartTask_Click(object sender, EventArgs e)
+        {
+            //  Create cancellationTokenSource within a button so it's lifetime will be accessible to both buttons
+            cancellationTokenSource = new CancellationTokenSource();
+            //  start a task
+            //  Task.Facotry.StartNew(Action, CancellationToken)
+            Task task1 = Task.Factory.StartNew(() =>
+            {
+                Random rand = new Random();
+                for(int i=0; i < 200; i++)
+                {
+                    //  here is where most of the work is being done so this is good candidate for CheckIfCancellation request
+                    //  check if cancellation has been requested
+                    if (cancellationTokenSource.Token.IsCancellationRequested)
+                    {
+                        //  do some cleanup like close files, net connections, db connections, streams, ...
+                        cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                    }
+                    WriteTextSafe(rand.Next().ToString(), listBox1);
+                    Thread.SpinWait(20_000_000);
+                }
+            }, cancellationTokenSource.Token);
+        }
+
+        private void btnCancelTask_Click(object sender, EventArgs e)
+        {
+            if(cancellationTokenSource != null)
+            {
+                //  request cancellation
+                cancellationTokenSource.Cancel();
+                WriteTextSafe("Operation cancelled.", listBox1);
+            }
+        }
+    }
+}
