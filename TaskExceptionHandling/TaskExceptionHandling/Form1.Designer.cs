d6d0a541886196b18a49e5dd54a522e1099bedde
Jon Rumsey <jon.rumsey@gmail.com>
2020-02-03 21:15:40 -0800

    In Class exploring aggregate exception handling.

---
+++ TaskExceptionHandling/TaskExceptionHandling/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace TaskExceptionHandling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnHandlingAttachedChildTaskExceptions = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.listBox2);
+            this.panel1.Controls.Add(this.btnHandlingAttachedChildTaskExceptions);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1421, 795);
+            this.panel1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(13, 117);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(730, 526);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnHandlingAttachedChildTaskExceptions
+            // 
+            this.btnHandlingAttachedChildTaskExceptions.Location = new System.Drawing.Point(13, 679);
+            this.btnHandlingAttachedChildTaskExceptions.Name = "btnHandlingAttachedChildTaskExceptions";
+            this.btnHandlingAttachedChildTaskExceptions.Size = new System.Drawing.Size(331, 102);
+            this.btnHandlingAttachedChildTaskExceptions.TabIndex = 1;
+            this.btnHandlingAttachedChildTaskExceptions.Text = "Handling Attached Child Tasks Exceptions";
+            this.btnHandlingAttachedChildTaskExceptions.UseVisualStyleBackColor = true;
+            this.btnHandlingAttachedChildTaskExceptions.Click += new System.EventHandler(this.btnHandlingAttachedChildTaskExceptions_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 29;
+            this.listBox2.Location = new System.Drawing.Point(759, 117);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(649, 526);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(8, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1400, 76);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1446, 820);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnHandlingAttachedChildTaskExceptions;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TaskExceptionHandling/TaskExceptionHandling/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskExceptionHandling
+{
+    public partial class Form1 : Form
+    {
+        private delegate void SafeCallDelegate(string text, ListBox listBox);
+        private delegate void SafeLabelDelegate(string text, Label label);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHandlingAttachedChildTaskExceptions_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            Task ParentTask = Task.Factory.StartNew(() =>
+           {
+               //   parent Task
+               //   create an inner Task
+               Task ChildTask1 = Task.Factory.StartNew(() =>
+              {
+                  //   Child Task1
+                  while (true)
+                  {
+                      //    get a random value
+                      int x = rand.Next(-999, 1000);
+                      Thread.SpinWait(rand.Next(100_000_000, 300_000_000));
+                      WriteTextSafe($"ChildTask1: x={ x }", listBox1);
+                      if (x < -500 && x % 2 != 0)
+                      {
+                          WriteTextSafe($"ChildTask1 is throwing an Arithmetic Exception!", listBox1);
+                          throw new ArithmeticException("ChildTask1 Error! Negative odd numbers are invalid");
+                      }
+                  }
+              });
+               Task ChildTask2 = Task.Factory.StartNew(() =>
+               {
+                   //   Child Task 2
+                   while (true)
+                   {
+                       //    get a random value
+                       int x = rand.Next(-999, 1000);
+                       Thread.SpinWait(rand.Next(100_000_000, 300_000_000));
+                       WriteTextSafe($"ChildTask1: x={ x }", listBox2);
+                       if (x > 500 && x % 2 == 0)
+                       {
+                           WriteTextSafe($"ChildTask2 is throwing an Argument Exception!", listBox2);
+                           throw new ArgumentException("ChildTask2 Error! Even numbers above 499 are invalid");
+                       }
+                   }
+               });
+               //  Add a Wait statement causing the Parent to wait for capturing the Aggregate Exception
+               //  All Exceptions thrown from Child Task(s) are wrapped inside the AggregateException instance
+               try
+               {
+                   //  wait on both children
+                   //  You *could* childTask1.Wait(); childTask2.Wait(); ... etc
+                   //  but preferred way is :
+                   Task.WaitAll(ChildTask1, ChildTask2);
+               }
+               catch (AggregateException ae)
+               {
+                   //   Sequence through the innerExceptions of ae
+                   foreach(Exception exception in ae.InnerExceptions)
+                   {
+                       //   Display the type and message of each innerException thrown by Child Tasks
+                       WriteLabelSafe($"{ exception.GetType().Name }: { exception.Message }\n", label1);
+                   }
+               }
+           });
+        }
+        private void WriteTextSafe(string text, ListBox listBox)
+        {
+            if (listBox.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(WriteTextSafe);
+                listBox.Invoke(d, new object[] { text, listBox });   //  can use 'this' instead of 'listBox'
+            }
+            else
+            {
+                listBox.Items.Add(text);
+            }
+        }
+        private void WriteLabelSafe(string text, Label label)
+        {
+            if (label.InvokeRequired)
+            {
+                var d = new SafeLabelDelegate(WriteLabelSafe);
+                label.Invoke(d, new object[] { text, label });
+            }
+            else
+            {
+                label.Text += text;
+            }
+        }
+    }
+}
