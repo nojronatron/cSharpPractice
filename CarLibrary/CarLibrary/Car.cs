93f52635b4f121d34b7a19179df901660ac5356b
Jon Rumsey <jon.rumsey@gmail.com>
2020-04-11 17:09:42 -0700

    Added Music Player method, incremented to v2.0.0.0.

--- CarLibrary/CarLibrary/Car.cs
+++ CarLibrary/CarLibrary/Car.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CarLibrary
 {
     //  represents the state of the engine
     public enum EngineState
     { engineAlive, engineDead }
+    //  represents different music media available in a Car
+    public enum MusicMedia
+    { musicCd, musicTape, musicRadio, musicMp3 }
     //  the abstract class Car
     public abstract class Car
     {
@@ -20,12 +24,17 @@ namespace CarLibrary
         public EngineState EngineState => egnState;
         public abstract void TurboBoost();
 
-        public Car() { }
+        public Car() => MessageBox.Show("CarLibrary Version 2.0!");
         public Car(string name, int maxSp, int currSp)
         {
+            MessageBox.Show("CarLibrary Version 2.0!");
             PetName = name;
             MaxSpeed = maxSp;
             CurrentSpeed = currSp;
+        }
+        public void TurnOnRadio(bool musicOn, MusicMedia mm)
+        {
+            MessageBox.Show(musicOn ? $"Jamming { mm }" : "Quiet time...");
         }
     }
 }
